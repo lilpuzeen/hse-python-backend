5a598d0e735a0d12197b312820159496f41315ba
Alex <aleksansra.stuckalowa@yandex.ru>
2024-10-12 22:13:27 +0300

    feat: remove xfail()

--- tests/lecture_4/test_example_parse_qs_non_parametrized.py
+++ tests/lecture_4/test_example_parse_qs_non_parametrized.py
@@ -1,5 +1,3 @@
-import pytest
-
 from lecture_4.example_parse_qs import parse_qs
 
 
@@ -15,21 +13,18 @@ def test_parse_qs_valid_2() -> None:
     assert result == {"name": "John", "age": "30"}
 
 
-@pytest.mark.xfail()
 def test_parse_qs_valid_3() -> None:
     query_string = "name=John&age=30&city=New%20York"
     result = parse_qs(query_string)
     assert result == {"name": "John", "age": "30", "city": "New York"}
 
 
-@pytest.mark.xfail()
 def test_parse_qs_valid_4() -> None:
     query_string = "name=John&age=30&city=New%20York&key="
     result = parse_qs(query_string)
     assert result == {"name": "John", "age": "30", "city": "New York", "key": ""}
 
 
-@pytest.mark.xfail()
 def test_parse_qs_valid_5() -> None:
     query_string = "name=John&name=Mary"
     result = parse_qs(query_string)
